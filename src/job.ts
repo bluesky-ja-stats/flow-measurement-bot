d6bd4beac185df435a47b7caaeb6d1f4de0c7d88
raitako-1 <raiji.kure551.naitoki@gmail.com>
2025-07-29 18:49:09 +0900

    いいねの測定に対応

--- src/job.ts
+++ src/job.ts
@@ -1,45 +1,83 @@
 import { AtpAgent } from '@atproto/api'
 import { WebSocket } from 'ws'
-import { type JetstreamEvent } from './types'
+import { JetstreamRecord, type JetstreamEvent } from './types'
 import { env } from './util/config'
 import { type Logger } from './util/logger'
 
 export const main = async (agent: AtpAgent, logger: Logger): Promise<void> => {
   const method = 'subscribe'
-  const query = 'wantedCollections=app.bsky.feed.post'
+  const wantedCollections = ['app.bsky.feed.post', 'app.bsky.feed.like']
+  const query = wantedCollections.map(v => 'wantedCollections='+v).join('&')
   const url = `${env.JETSTREAM_ENDPOINT}/${method}?${query}`
-  const allCursor: number[] = []
-  const jaCursor: number[] = []
+  const cursors: {
+    all: number[]
+    posts: {
+      all: number[]
+      ja: number[]
+    }
+    likes: {
+      all: string[]
+      ja: string[]
+    }
+  } = {
+    all: [],
+    posts: {
+      all: [],
+      ja: [],
+    },
+    likes: {
+      all: [],
+      ja: [],
+    },
+  }
   const ws = new WebSocket(url)
   logger.info(`Jetstream: ${url}`)
   ws.on('open', () => {})
   ws.on('message', (data) => {
     const event = JSON.parse(data.toString()) as JetstreamEvent
-    if (allCursor.length === 0 || event.time_us < (allCursor[0] + 60*(10**6))) {
-      if (event.kind === 'commit' && event.commit.operation === 'create' && event.commit.record.$type === 'app.bsky.feed.post' && event.commit.record.text) {
-        allCursor.push(event.time_us)
-        var searchtext: string = event.commit.record.text
-        if (event.commit.record.embed?.images && Array.isArray(event.commit.record.embed.images)) {
-          for (const image of event.commit.record.embed.images) {
-            searchtext = searchtext + image.alt
+    if (cursors.all.length === 0 || event.time_us < (cursors.all[0] + 60*(10**6))) {
+      cursors.all.push(event.time_us)
+      if (event.kind === 'commit'){
+        if (event.commit.operation === 'create') {
+          if (event.commit.record.$type === 'app.bsky.feed.post' && event.commit.record.text) {
+            cursors.posts.all.push(event.time_us)
+            if (isJa(event.commit.record)) cursors.posts.ja.push(event.time_us)
+          } else if (event.commit.record.$type === 'app.bsky.feed.like') {
+            cursors.likes.all.push(event.commit.record.subject.uri)
           }
         }
-        if ((typeof event.commit.record.langs !== 'undefined' && event.commit.record.langs.includes('ja')) || (searchtext.match(/^.*[ぁ-んァ-ヶｱ-ﾝﾞﾟー]+.*$/))) {
-          jaCursor.push(event.time_us)
-        }
+      } else if (event.kind === 'identity') {
+      } else if (event.kind === 'account') {
       }
     } else {
       ws.close()
     }
-    logger.debug('処理終了')
   })
   ws.on('error', (error) => {})
   ws.on('close', async (code, reason) => {
-    logger.debug('close処理開始')
-    const d = new Date(allCursor[0]/(10**3))
+    logger.debug('close処理')
+    const size = 25
+    for (const sepLikes of cursors.likes.all.flatMap((_, i, a) => i % size ? [] : [a.slice(i, i + size)])) {
+      const posts = (await agent.getPosts({uris: sepLikes})).data.posts
+      for (const post of posts) if (isJa(post.record)) cursors.likes.ja.push(post.uri)
+    }
+    const d = new Date(cursors.all[0]/(10**3))
     const date = `${d.getFullYear()}-${String(d.getMonth()+1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')} ${String(d.getHours()).padStart(2, '0')}:${String(d.getMinutes()).padStart(2, '0')}:${String(d.getSeconds()).padStart(2, '0')} GMT+0900 (日本標準時)`
-    const text = `${date}\nからの1分間で受信したBlueskyの投稿数は以下の通りです\n\n日本語: ${jaCursor.length} [post/min]\n全投稿: ${allCursor.length} [post/min]`
-    await agent.post({text})
+    const text = `測定開始: ${date}\n測定時間: 1分\n\n日本語を含む投稿: ${cursors.posts.ja.length} [post/min]\n全ての投稿　　　: ${cursors.posts.all.length} [post/min]\n\n日本語を含む投稿へのいいね: ${cursors.likes.ja.length} [like/min]\n全てのいいね　　　　　　　: ${cursors.likes.all.length} [like/min]`
+    await agent.post({text, langs: ['ja']})
     logger.info(text)
   })
+}
+
+const isJa = (record: any): boolean => {
+  var searchtext: string = record.text
+  if (record.embed?.images && Array.isArray(record.embed.images)) {
+    for (const image of record.embed.images) {
+      searchtext = searchtext + image.alt
+    }
+  }
+  if ((typeof record.langs !== 'undefined' && record.langs.includes('ja')) || (searchtext.match(/^.*[ぁ-んァ-ヶｱ-ﾝﾞﾟー]+.*$/))) {
+    return true
+  }
+  return false
 }

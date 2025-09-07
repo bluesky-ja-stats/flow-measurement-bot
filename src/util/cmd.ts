ff34d060818ec04367b8542a6c2af42e3768799b
raitako-1 <raiji.kure551.naitoki@gmail.com>
2025-09-08 05:05:45 +0900

    Update dependencies

--- src/util/cmd.ts
+++ src/util/cmd.ts
@@ -9,7 +9,6 @@ export const setupCmd = (bot: Bot, logger: Logger) => {
     const cmd = (data.toString()).split(' ').map(v => v.trim()).filter(v => v !== '')
     for (let i = cmd.length; i > 0; i--) {
       const fullPath = path.join(__dirname, `../cmds/${cmd.slice(0, i).join('/')}${path.parse(__filename).ext}`)
-      console.log(fullPath)
       if (fs.existsSync(fullPath)) {
         const event = (await import(fullPath)).default as (bot: Bot, logger: Logger, args: string[]) => Promise<void>
         await event(bot, createLogger(['Runner', 'Commander'].concat(cmd.slice(0, i))), cmd.slice(i))
